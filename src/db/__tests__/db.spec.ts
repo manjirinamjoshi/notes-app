04edc194258904c9b6749a4ee2968e7f90c67816
Manjiri Namjoshi <mnamjoshi@paciolan.com>
2021-06-24 01:22:36 -0700

    db unit test update

--- src/db/__tests__/db.spec.ts
+++ src/db/__tests__/db.spec.ts
@@ -5,7 +5,6 @@ const successBody = {
     prefix: "TEST"
 };
 
-const notesId: any = "12345fsdfsdfsf";
 describe("DB Unit Test", () => {
     afterEach(() => {
         sinon.restore();
@@ -20,7 +19,7 @@ describe("DB Unit Test", () => {
         expect(allNotes.length).toEqual(1);
 
         // cleanup
-        await db.deleteNotes([allNotes.id], "456");
+        await db.deleteNotes([allNotes[0].id], "456");
 
     }, 10000);
 
@@ -48,14 +47,76 @@ describe("DB Unit Test", () => {
         expect(allNotes.length).toEqual(1);
 
         // attempt to delete note for userId 999
-        await db.deleteNotes([allNotes.id], "999");
+        await db.deleteNotes([allNotes[0].id], "999");
 
         // note still exists for user 123
         allNotes = await db.getAllNotes("123");
         expect(allNotes.length).toEqual(1);
 
         // cleanup
-        await db.deleteNotes([allNotes.id], "123");
+        await db.deleteNotes([allNotes[0].id], "123");
+
+    }, 10000);
+
+    test("note is updated successfully", async () => {
+        await db.createNote({
+            id: "test",
+            title: "test title",
+            userId: "123"
+        });
+        let allNotes = await db.getAllNotes("123");
+        expect(allNotes.length).toEqual(1);
+        expect(allNotes[0].title).toEqual("test title");
+
+        await db.updateNote({
+            id: "test",
+            userId: "123",
+            title: "update test title",
+            content: "update test content"
+        })
+
+        // note still exists for user 123
+        allNotes = await db.getAllNotes("123");
+        expect(allNotes.length).toEqual(1);
+        expect(allNotes[0].title).toEqual("update test title");
+        expect(allNotes[0].content).toEqual("update test content");
+
+        // cleanup
+        await db.deleteNotes([allNotes[0].id], "123");
+
+    }, 10000);
+
+    test("searching by prefix returns a note for a match in title", async () => {
+        await db.createNote({
+            id: "test",
+            title: "test title",
+            searchWords: ["test", "title"],
+            userId: "123"
+        });
+        const searchResults = await db.searchNotes("ti", "123");
+        expect(searchResults.length).toEqual(1);
+        expect(searchResults[0].title).toEqual("test title");
+
+        // cleanup
+        await db.deleteNotes([searchResults[0].id], "123");
+
+    }, 10000);
+
+    test("searching by prefix returns a note for a match in content", async () => {
+        await db.createNote({
+            id: "test",
+            title: "test title",
+            content: "abc def ghi jkl",
+            searchWords: ["test", "title", "abc", "def", "ghi", "jkl"],
+            userId: "123"
+        });
+        const searchResults = await db.searchNotes("def", "123");
+        expect(searchResults.length).toEqual(1);
+        expect(searchResults[0].title).toEqual("test title");
+        expect(searchResults[0].content).toEqual("abc def ghi jkl");
+
+        // cleanup
+        await db.deleteNotes([searchResults[0].id], "123");
 
     }, 10000);
 });
